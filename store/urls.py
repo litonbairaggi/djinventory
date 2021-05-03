0d4f6d123fe99ef7e80b4cb2899dbbb160cd3eb6
litonbairaggi <litonovi2013@gmail.com>
2021-05-04 01:10:13 +0600

    product update complite

--- store/urls.py
+++ store/urls.py
@@ -12,8 +12,9 @@ from .views import (
 
 urlpatterns = [
     path('create-product/', views.create_product, name='create-product'),
-    
+
     path('product-list/', views.ProductListView.as_view(), name='product-list'),
+    path('product-edit/<int:pk>/edit/', views.ProductUpdateView.as_view(), name='product-edit'),
 ]
 
 
--- store/views.py
+++ store/views.py
@@ -2,11 +2,23 @@ from django.shortcuts import render, redirect
 
 #from django.views.generic import TemplateView
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DetailView,
+    DeleteView,
+    View,
+)
 
 from .models import Product
 
 from . froms import ProductForm
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'store/list_product.html'
+    context_object_name = 'products'
 
 
 # Product views
@@ -23,8 +35,7 @@ def create_product(request):
     }
     return render(request, 'store/create_product.html', context)
 
-
-class ProductListView(ListView):
-    model = Product.objects.all()
-    template_name = 'store/product_list.html'
-    context_object_name = 'product'
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ['name', 'product_code', 'bying_price', 'selling_price']
+    template_name = 'store/edit_product.html'
